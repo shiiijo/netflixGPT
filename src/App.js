f7ae0079c4921fe553ebcc1765fca10798eb0214
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-08-29 00:20:02 +0530

    added initial basic componenets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="bg-red-400 text-center">Hi , everyone</h1>
-    </div>
+    <Body/>
   );
 }
 
