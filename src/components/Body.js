985dc04590fdd31b3f419ef5fc16fe12392d31f5
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-09-08 00:11:48 +0530

    about onAuthStateChanged api

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,8 @@ import Header from "./Header";
 import Login from "./Login";
 import Browse from "./Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import auth from "../utils/firebase";
 
 const Body = () => {
   const appRouter = createBrowserRouter([
@@ -15,6 +17,20 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
+
+  React.useEffect(() => {
+    // it is a event listner api given by firebase , it does what is defined inside when user state[signin, signout , signup] changes
+    // in our case we will dispatch redux action when user state changes to save user data & to delete user data from redux store
+    // it is defined here because we want to make data consistant for whole app
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed-up & signed-in
+        const uid = user.uid;
+      } else {
+        // User is signed out
+      }
+    });
+  }, []);
 
   return (
     <div>
