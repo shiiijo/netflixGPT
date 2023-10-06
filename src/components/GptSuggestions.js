176052bf6e8a5f4bd10b7c0493be6101e4fb56d4
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-10-06 22:49:03 +0530

    added loader

--- src/components/GptSuggestions.js
+++ src/components/GptSuggestions.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
+import ReactLoading from "react-loading";
 
 const GptSuggestions = () => {
   const suggestedMovies = useSelector((store) => store.gpt.gptSuggestions);
   const movieKeywords = useSelector((store) => store.gpt.movieKeywords);
-  console.log("@@@@@", suggestedMovies);
-  console.log("&&&&&&", movieKeywords);
-  return (
-    suggestedMovies && (
-      <div className="bg-black m-16">
-        {suggestedMovies.map((movie, idx) => (
-          <MovieList title={movieKeywords[idx]} movies={movie.results} />
-        ))}
-      </div>
-    )
+  return !suggestedMovies ? (
+    <ReactLoading
+      className="m-auto mt-24"
+      type="bars"
+      color="white"
+      height={300}
+      width={150}
+    />
+  ) : (
+    <div className="bg-black m-16">
+      {suggestedMovies.map((movie, idx) => (
+        <MovieList title={movieKeywords[idx]} movies={movie.results} />
+      ))}
+    </div>
   );
 };
 
